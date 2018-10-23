89ed7861ae3d3fd933cd94a8eadc1cde8dadf9d5
Mahesh De Silva <maheshd@irononetech.com>
2018-10-23 11:25:30 +0530

    Add Store class.

---
+++ app/src/main/java/testapp/irononetech/com/testgithubapp/Store.java
@@ -0,0 +1,13 @@
+package testapp.irononetech.com.testgithubapp;
+
+public class Store {
+    private String storeName;
+    private int storeId;
+    private String countryCode;
+
+    public Store(String storeName, int storeId, String countryCode){
+        this.storeName = storeName;
+        this.storeId = storeId;
+        this.countryCode = countryCode;
+    }
+}
